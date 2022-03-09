c93c7bda9331f8d6b577212eea10b22c9c8a5024
Victor Gonzalez <victor@vgr.cl>
2022-03-09 15:19:45 -0300

    added simple layout
    Signed-off-by: Victor Gonzalez <victor@vgr.cl>

---
+++ components/background.tsx
@@ -0,0 +1,15 @@
+import Image from 'next/image'
+import bg from '../images/bg.png'
+
+
+export default function Background() {
+    return (
+        <>
+        <div className="absolute z-20 top-0 inset-x-0 flex justify-center overflow-hidden pointer-events-none">
+            <div className="w-[108rem] flex-none flex justify-end">
+                <Image src={bg} alt="background image" className='w-[90rem] flex-none max-w-none block' />
+            </div>
+        </div>
+        </>
+    )
+}
